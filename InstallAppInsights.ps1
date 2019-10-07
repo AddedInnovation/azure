afb798f882e7e016180d3b6633dcb7ed5ca3e2ad
Mark Whisler <mwhisler@addedinnovation.com>
2019-10-07 11:19:14 -0500

    Update InstallAppInsights.ps1

--- InstallAppInsights.ps1
+++ InstallAppInsights.ps1
@@ -3,6 +3,11 @@ param(
 	$key
 )
 
-Write-Information "Going to install application insights"
-Write-Information $key
-Mkdir C:\AppX$key
+function InstallAppInsights([String]$InstrumentationKey)
+{
+	Write-Information "Going to install application insights"
+	Write-Information $key
+	Mkdir C:\AppX$key   
+}
+
+InstallAppInsights $key
